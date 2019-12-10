3510a52d6152ee86023a76ad8ab994436ab14d88
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-12-10 22:57:08 +0100

    add react-pdf

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Document } from "react-pdf";
 
 function App() {
   return (
